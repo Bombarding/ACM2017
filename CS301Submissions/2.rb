a76f43e0a71749301d1dd7fa3735eed9660b27d7
Bombarding <skruffy@aim.com>
2017-03-05 16:11:01 -0500

    650$

---
+++ CS301Submissions/2.rb
@@ -0,0 +1,19 @@
+a,b,c = gets.split
+y,x,w,v,u = gets.split
+
+r=[a,b,c]
+t=[y,x,w,v,u]
+def f(a)
+	g=e[i+1]-e[i]+1
+	h=j < k ? k : j
+	l = e[i+1]+e[i]-1
+	m = n > o ? o : n
+	r << [h,m] 	
+	r.sort
+end
+
+ans = r[0]
+def f(r)
+	r=r[i]
+	ans<<r
+end
